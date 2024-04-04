ec1518ed6c6525e432c053f707be12be474a4b6d
pbarchwic <barchwicp@gmail.com>
2024-04-04 21:39:19 +0200

    add card component

--- src/components/product-card/product-card.jsx
+++ src/components/product-card/product-card.jsx
@@ -1,53 +1,38 @@
 // import { useState, useEffect } from "react";
 // import axios from 'axios';
+import './product-card.scss'
 import data from '../../assets/data-mock/products.json'
 
 function ProductCard() {
 
-    // const [data, setData] = useState([]);
-
-    // useEffect(() => {
-    //   const fetchData = async () => {
-    //     try {
-    //       const response = await axios.get('/src/assets/data-mock/products.json');
-    //       setData(response.data);
-    //       console.log(response.data);
-    //     } catch (error) {
-    //       console.error('Błąd podczas pobierania danych:', error);
-    //     }
-    //   };
-  
-    //   fetchData();
-    // }, []);
-
-    console.log(data)
-
-    return (
-        <div class="products">
-            {/* <div class="product-card" *ngFor="let product of products">
-                <img class="product-card__img" [src]="product.image" />
-                <div class="product-card__desc" [class.disabled]="product.owned">
-                <span class="name">{{ product.name }}</span>
+    console.log(data);
+    const products = data.products;
+    const productCard = products.map((product, index) => (
+        <div className="product-card">
+            <img className="product-card__img" src={product.image} key={index} alt={product.name}/>
+            <div className="product-card__desc" disabled={product.owned}>
+                <span class="name">{product.name}</span>
                 <div class="info-box">
-                    <span class="discount" *ngIf="product.discount"
-                    >-{{ product.discount }}%</span
-                    >
+                    <span class="discount" >-{product.discount}%</span>
                     <button
                     class="button action"
-                    *ngIf="!product.owned; else owned"
-                    (click)="addToCart(product)"
-                    [disabled]="product.inCart"
+                    //  *ngIf="!product.owned; else owned"
+                    //  (click)="addToCart(product)"
+                    //  [disabled]="product.inCart"
                     >
-                    {{ product.inCart ? "in cart" : "$" + product.price }}
+                        {product.inCart ? "in cart" : "$" + product.price }
                     </button>
-                    <ng-template #owned>
+                    {/* <ng-template #owned>
                     <span class="action">owned</span>
-                    </ng-template>
+                    </ng-template> */}
                 </div>
-                </div>
-            </div> */}
+            </div>
+        </div>
+    ))
 
-            produkty
+    return (
+        <div className="products">
+            {productCard}
         </div>
 
     )
